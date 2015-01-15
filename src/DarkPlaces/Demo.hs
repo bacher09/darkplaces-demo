911d367f301c37c5af6b3811e94c5b27fb2e7398
Slava Bacherikov <slava@bacher09.org>
2015-01-15 14:11:58 +0200

    Add iterDemoMessages to Demo module

--- src/DarkPlaces/Demo.hs
+++ src/DarkPlaces/Demo.hs
@@ -1,15 +1,17 @@
 module DarkPlaces.Demo (
     getDemoMessage,
-    getDemoMessages
+    getDemoMessages,
+    iterDemoMessages
 ) where
 import Control.Applicative
-import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString as B
 import Data.Binary.Get
 import DarkPlaces.Types
 import DarkPlaces.Binary
 
 
-getDemoMessage :: Get (QVector, L.ByteString)
+getDemoMessage :: Get (QVector, BL.ByteString)
 getDemoMessage = do
     size <- fromIntegral <$> getWord32le
     angls <- getQVector
@@ -17,9 +19,22 @@ getDemoMessage = do
     return (angls, msg)
 
 
-getDemoMessages :: Get [(QVector, L.ByteString)]
+getDemoMessages :: Get [(QVector, BL.ByteString)]
 getDemoMessages = do
     empty <- isEmpty
     if empty
         then return []
         else (:) <$> getDemoMessage <*> getDemoMessages
+
+
+iterDemoMessages :: BL.ByteString -> [Either (ByteOffset, String) (QVector, BL.ByteString)]
+iterDemoMessages demo_data = go decoder $ BL.toChunks demo_data
+  where
+    decoder = runGetIncremental getDemoMessage
+    go (Fail _ offset msg) _ = [Left (offset, msg)]
+    go (Partial k) [] = go (k Nothing) []
+    go (Partial k) (x:xs) = go (k $ Just x) xs
+    go (Done left _ res) xs = Right res : if empty then [] else go decoder xs'
+      where
+        empty = B.null left && null xs
+        xs' = left:xs
